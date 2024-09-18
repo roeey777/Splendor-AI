e2faf17a07422686fff780c6e245d10be9c2b489
Eyal Royee <eyalroyee@gmail.com>
2024-09-18 16:15:34 +0300

    removed some more redundent comments.
    Signed-off-by: Eyal Royee <eyalroyee@gmail.com>

--- src/Engine/agents/our_agents/ppo/ppo_agent.py
+++ src/Engine/agents/our_agents/ppo/ppo_agent.py
@@ -12,19 +12,17 @@ from functools import partial
 class myAgent(Agent):
     def __init__(self, _id):
         super().__init__(_id)
-        # load_weights
+
         env = gym.make("splendor-v1", agents=[])
+
+        # load_weights
         self.net = PPO(env.observation_space.shape[0], env.action_space.n, DROPOUT)
-        # PPO = partial(ppo, input_dim=env.observation_space.shape[0],
-        #               output_dim=env.action_space.n, dropout=0)
         torch.serialization.add_safe_globals([PPO])
         self.net = torch.load(
             str(Path(__file__).parent / f"ppo_model.pth"),
             weights_only=True,
             map_location="cpu",
         )
-        # self.net.load_state_dict(torch.load(str(Path(__file__).parent /
-        #                            f"ppo_model.pth"), weights_only=False))
         self.net.eval()
 
     def SelectAction(self, actions, game_state, game_rule):
