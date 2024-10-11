3a478ccd6a57fc16d00e64cab00b5579ef2343ca
Eyal Royee <eyalroyee@gmail.com>
2024-10-11 16:59:53 +0300

    PPO: add a PPO model incorporating a self-attention mechanism.
    Signed-off-by: Eyal Royee <eyalroyee@gmail.com>

---
+++ docs/source/splendor.agents.our_agents.ppo.self_attn.rst
@@ -0,0 +1,37 @@
+splendor.agents.our\_agents.ppo.self\_attn package
+==================================================
+
+Submodules
+----------
+
+splendor.agents.our\_agents.ppo.self\_attn.constants module
+-----------------------------------------------------------
+
+.. automodule:: splendor.agents.our_agents.ppo.self_attn.constants
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+splendor.agents.our\_agents.ppo.self\_attn.network module
+---------------------------------------------------------
+
+.. automodule:: splendor.agents.our_agents.ppo.self_attn.network
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+splendor.agents.our\_agents.ppo.self\_attn.ppo\_agent module
+------------------------------------------------------------
+
+.. automodule:: splendor.agents.our_agents.ppo.self_attn.ppo_agent
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: splendor.agents.our_agents.ppo.self_attn
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- src/splendor/agents/our_agents/ppo/arguments_parsing.py
+++ src/splendor/agents/our_agents/ppo/arguments_parsing.py
@@ -8,11 +8,19 @@ from splendor.version import get_version
 from splendor.agents.generic.random import myAgent as RandomAgent
 from splendor.agents.our_agents.minmax import myAgent as MinMaxAgent
 
+# recurrent PPO with GRU
 from .ppo_rnn.gru.network import PPO_GRU
 from .ppo_rnn.gru.network import HIDDEN_STATE_DIM as GRU_HIDDEN_STATE_DIM
 from .ppo_rnn.gru.ppo_agent import DEFAULT_SAVED_PPO_GRU_PATH
+
+# PPO with self-attention
+from .self_attn.network import PPOSelfAttention
+from .self_attn.ppo_agent import DEFAULT_SAVED_PPO_SELF_ATTENTION_PATH
+
+# vanilla PPO
 from .network import PPO
 from .ppo_agent import DEFAULT_SAVED_PPO_PATH
+
 from .ppo_base import PPOBaseFactory
 from .constants import (
     SEED,
@@ -42,6 +50,9 @@ NN_ARCHITECTURES = {
     "mlp": NeuralNetArch("ppo_mlp", PPO, False, DEFAULT_SAVED_PPO_PATH),
     "gru": NeuralNetArch(
         "ppo_gru", PPO_GRU, True, DEFAULT_SAVED_PPO_GRU_PATH, GRU_HIDDEN_STATE_DIM
+    ),
+    "self_attn": NeuralNetArch(
+        "ppo_self_attn", PPOSelfAttention, False, DEFAULT_SAVED_PPO_SELF_ATTENTION_PATH
     ),
 }
 NN_ARCHITECTURES_CHOICES = NN_ARCHITECTURES.keys()
