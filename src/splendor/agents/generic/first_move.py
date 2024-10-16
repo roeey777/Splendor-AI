b5fcfb321714cb2ca68a05a03f59cb3e87ad1184
Eyal Royee <eyalroyee@gmail.com>
2024-10-16 22:38:34 +0300

    Generic Agents: passing pylint
    Signed-off-by: Eyal Royee <eyalroyee@gmail.com>

--- src/splendor/agents/generic/first_move.py
+++ src/splendor/agents/generic/first_move.py
@@ -1,9 +1,22 @@
+"""
+Implementation of an agent that selects the first legal action.
+"""
+
+from typing import override
+
 from splendor.template import Agent
 
 
-class myAgent(Agent):
-    def __init__(self, _id):
-        super().__init__(_id)
+class FirstActionAgent(Agent):
+    """
+    An agent that selects the first legal action.
+    """
 
+    # pylint: disable=too-few-public-methods
+
+    @override
     def SelectAction(self, actions, game_state, game_rule):
         return actions[0]
+
+
+myAgent = FirstActionAgent  # pylint: disable=invalid-name
